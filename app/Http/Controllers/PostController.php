52156653d25228d655ea52b58c17791a1cb23475
andialif2015 <13020180157@umi.ac.id>
2021-05-04 15:51:15 +0800

    menghubungkan database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function show($slug){
-        return view('posts.show', compact('slug'));
+        $post = \DB::table('posts')->where('slug', $slug)->first();
+
+        return view('posts.show', compact('post'));
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
-@section('title', $slug)
+@section('title', $post->slug)
 @section('content')
     <div class="container">
-        {{ $slug }}
+        <h1> {{ $post->title }} </h1>
+        <p> {{ $post->body }} </p>
     </div>
 @endsection
